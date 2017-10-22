319e9ef6abb270e581a0ee25b663af56fb76bc78
Lee <pflee@nala.roche.com>
2017-10-21 17:53:32 -0700

    modal made me cry, order of CDNs listed do matter

--- server.js
+++ server.js
@@ -14,14 +14,27 @@
 	var app = express();
 	
 	// Sets an initial port. use later in our listener
+	//In environments (e.g. Heroku), and as convention, set the environment variable PORT to tell your web server what port to listen on.
+    //process.env.PORT || 3000 means: whatever is in the environment variable PORT, or 3000 if there's nothing there.
+
+
 	var PORT = process.env.PORT || 8080;
 	
 	// BodyParser makes it possible for our server to interpret data sent to it.
-	// The code below is pretty standard.
+	//BP is middleware - software that acts as a bridge between an operating system or database and applications, especially on a network.
+	// The code below is provided at https://www.npmjs.com/package/body-parser
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(bodyParser.text());
 	app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+	//application/vnd.api+json - specification for how a client should request that resources be fetched or modified, and how a server should respond to those requests.
+	//is API specific media type. The vendor prefix (vnd.) indicates that it is custom for this vendor. 
+	//+ json indicates that it can be parsed as JSON, but the media type should define further semantics on top of JSON.
+
+	
+
+	//application/json just means that the content is JSON. Not useful, it does not define what the JSON values mean.
 	
 	app.use('/static', express.static(path.join(__dirname, 'app/public')))
 			
@@ -30,8 +43,8 @@
 	// These route files/routes when users visit or request data from various URLs.
 	// ===========================================================================
 	
-	require("./app/routing/api-routes.js")(app);
-	require("./app/routing/html-routes.js")(app);
+	require("./app/routing/api-route.js")(app);
+	require("./app/routing/html-route.js")(app);
 	
 	
 	// LISTENER
